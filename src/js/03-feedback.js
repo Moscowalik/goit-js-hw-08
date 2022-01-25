2e02c6f3ffe60e4d4cdb62ca4a44998e7a5b227c
Moscowalik <moscowalik95@gmail.com>
2022-01-25 19:28:55 +0200

    Uptade 3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,16 +1,21 @@
 import throttle from 'lodash.throttle';
 
 const formRef = document.querySelector('.feedback-form');
-const formData = [];
+const formData = {};
+const STORAGE_KEY = 'feedback-form-state';
 
 formRef.addEventListener('submit', onFormSubmit);
 formRef.addEventListener('input', throttle(onTextAreaInput, 500));
 
 function onFormSubmit(e) {
-  console.log('Submit');
+  e.preventDefault();
+
+  e.currentTarget.reset();
 }
 
 function onTextAreaInput(e) {
   formData[e.target.name] = e.target.value;
   console.log(formData);
+
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
 }
